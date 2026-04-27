a41fbf7e3a373b95dc8980dcb862407d4e72faf7
Nasrulloh <ilmi.nasrulloh@gmail.com>
2026-04-27 11:14:27 +0700

    Third Commit

--- Mise/ContentView.swift
+++ Mise/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "applelogo")
+                .frame(width: 100, height: 100)
+                .background(Color.gray)
+                .cornerRadius(50)
                 .imageScale(.large)
                 .scaledToFill()
                 .font(Font.largeTitle.bold())
