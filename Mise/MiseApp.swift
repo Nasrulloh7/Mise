d2211e90b5437d6de341384b596ae42a54fb2955
Nasrulloh <ilmi.nasrulloh@gmail.com>
2026-04-27 09:50:32 +0700

    Initial Commit

---
+++ Mise/MiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiseApp.swift
+//  Mise
+//
+//  Created by M Ilmi Nasrulloh on 27/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
