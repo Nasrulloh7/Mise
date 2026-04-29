c45f91bd3cebfe77ccaad2d372ea5c798dd87c1e
KeziaKA <keziakaren63@gmail.com>
2026-04-29 11:35:45 +0700

    jgvbj

--- Mise/ContentView.swift
+++ Mise/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "applelogo")
+            Image(systemName: "hbih")
                 .frame(width: 100, height: 100)
                 .background(Color.blue)
                 .cornerRadius(50)
