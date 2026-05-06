28f544efc6af77bc309ad7371a2f1f587773bd1a
Nasrulloh <ilmi.nasrulloh@gmail.com>
2026-05-06 23:52:06 +0700

    Update Component TemplateCard and Preview OverlayView

--- Mise/Components/TemplateCard.swift
+++ Mise/Components/TemplateCard.swift
@@ -5,28 +5,52 @@ struct TemplateCard: View {
     let entry: TemplateEntry
     
     var body: some View {
-        VStack(spacing: 12) {
-            Image(entry.imageName)
-                .resizable()
-                .scaledToFit()
-                .cornerRadius(12)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color.white.opacity(0.2), lineWidth: 1)
-                )
+        ZStack {
+            Color.black.ignoresSafeArea()
             
-            Text(entry.title)
-                .font(.headline)
-                .foregroundColor(.white)
+            ZStack(alignment: .bottom) {
+                
+                Image(entry.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .clipped()
+                    .aspectRatio(contentMode: .fit)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(Color.white.opacity(0.2), lineWidth: 1)
+                    )
+                
+                HStack {
+                    Spacer()
+                    
+                    Text(entry.title)
+                        .font(.headline)
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+//                    Image(systemName: "info.circle")
+//                        .font(.headline)
+//                        .foregroundColor(.white)
+                    
+                }
+                .shadow(radius: 10)
+                .padding(10)
+                .glassEffect(.clear, in: .rect())
+                    
+            }
+            .cornerRadius(16)
         }
-        .padding(10)
-        .background(Color.white.opacity(0.05))
-        .cornerRadius(16)
     }
 }
 
 #Preview {
-    NavigationStack {
-        TemplateView(count: 2)
-    }
+    TemplateCard(entry: TemplateEntry(
+        title: "Diagonal",
+        imageName: "Dg-01-03",
+        objectCount: 3, guides:[
+        ObjectGuide(x: 444, y: -2, width: 615),
+        ObjectGuide(x: 553, y: 700, width: 403),
+        ObjectGuide(x: 261, y: 1040, width: 493)
+    ]))
 }
